0fe08689b16b128f2fed9783a97a8233b09282f9
Goldyga <marcingo@toya.net.pl>
2017-11-25 16:02:02 +0100

    added files

--- js/main.js
+++ js/main.js
@@ -1,42 +1,69 @@
-var url = 'https://api.icndb.com/jokes/random';
-var button = document.getElementById('get-joke');
-var paragraph = document.getElementById('joke');
+var url = 'https://restcountries.eu/rest/v2/name/';
+var countriesList = $('#countries');
+var input = $('#country-name');
 
-window.addEventListener('load', function() {
-	getJoke();
+$('#search').click(searchCountries);
+$(input).keypress(function(e) {
+    if(e.which == 13) {
+        searchCountries();
+    }
 });
 
-button.addEventListener('click', function(){
-  getJoke();
-});
-
-function getJoke() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.addEventListener('load', function(){
-    var response = JSON.parse(xhr.response);
-    paragraph.innerHTML = response.value.joke;
-  });
-  xhr.send();
+function searchCountries() {
+  	var countryName = $(input).val();
+  	if(!countryName.length) countryName = jumpBtn();
+  	$.ajax ({
+  		url: url + countryName,
+  		method: 'GET',
+  		statusCode: {
+    		404: function() {
+      			jumpBtn();
+    		}
+  		},
+  		success: showCountriesList
+  	});
 }
 
+function jumpBtn() {
+	$(input).css("animation", "jump .9s infinite");
+	$(input).val("Oops data no found or not entered");
+	function rmvAtrr () {
+		$(input).css("animation", "");
+		$(input).val("");
+	}
+	setTimeout(rmvAtrr, 2000);
+}
 
+function showCountriesList(resp) {
+	countriesList.empty();
+	$(input).val("");
 
-// var url = 'https://api.icndb.com/jokes/random';
-// var $paragraph = $('#joke');
+	resp.forEach(function(item) {
+		var infoElement = $('<div/>', {
+	    	class: "search__countries--box",
+		}).appendTo(countriesList);
+		$('<img/>', {
+	    	class: "flagImg",
+   	 		src: item.flag,
+		}).appendTo(infoElement);
+	    $('<p>').text("Country Name: " + item.name).appendTo(infoElement);
+	    $('<p>').text("Capital Name: " + item.capital).appendTo(infoElement);
+	    $('<p>').text("Population: " + item.population).appendTo(infoElement);
+	    var languages = item.languages;
+	    function showLanguages(languages) {
+	    	languages.forEach(function(la) {
+	    		$('<p>').text("Languages: " + la.name).appendTo(infoElement);
+	    	});
+	    }
+	    var currencies = item.currencies;
+	    function showCurrencies(currencies) {
+	    	currencies.forEach(function(cu) {
+	    		$('<p>').text("Currency: " + cu.name).appendTo(infoElement);
+	    	});
+	    }
+	    showCurrencies(currencies);
+	    showLanguages(languages);
+	 });
+}
 
-// var $button = $('#get-joke').click(function() {
-// 	getJoke();
-// });
-
-// function getJoke() {
-// $.ajax({
-// 	method: 'GET',
-// 	url: url,
-// 	success: function(res) {
-// 		$paragraph.text(res.value.joke);
-// 	}
-
-// });
-// }
 
